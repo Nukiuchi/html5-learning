dc15836eb93d8af19a8449eb2b8ccc406f3700ec
Nukiuchi <mikkosairo@gmail.com>
2018-01-23 19:07:28 +0200

    js planning

---
+++ 5calc/calc.js
@@ -0,0 +1,34 @@
+// --- Calc Class
+// current value
+// input value
+// memory slot
+// f add
+// f sub
+// f multi
+// f div
+
+// f exp
+// f inv
+// f sqrt
+// f perc
+
+// f m store
+// f m recall
+// f m clear 
+// f m add
+// f m sub
+// --- end Calc class
+
+// Initialise
+
+// Bind numbers
+// Bind operations
+// Bind formatting
+// Bind memory
+
+// Bind keypresses
+
+
+
+// Read input
+// Write output
